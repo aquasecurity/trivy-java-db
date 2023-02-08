cd862959307814c0b6021bfe68c53e35ad279161
knqyf263 <knqyf263@gmail.com>
2023-02-09 08:35:04 +0900

    refactor: use cobra

--- cmd/trivy-java-db/main.go
+++ cmd/trivy-java-db/main.go
@@ -2,11 +2,11 @@ package main
 
 import (
 	"context"
-	"flag"
 	"log"
 	"os"
 	"path/filepath"
 
+	"github.com/spf13/cobra"
 	"golang.org/x/xerrors"
 
 	"github.com/aquasecurity/trivy-java-db/pkg/crawler"
@@ -15,55 +15,75 @@ import (
 )
 
 func main() {
-	if err := run(); err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		log.Fatalf("%+v", err)
 	}
 }
 
-func run() error {
+var (
+	// Used for flags.
+	cacheDir string
+	limit    int
+
+	rootCmd = &cobra.Command{
+		Use:   "trivy-java-db",
+		Short: "Build Java DB to store maven indexes",
+	}
+	crawlCmd = &cobra.Command{
+		Use:   "crawl",
+		Short: "Crawl maven indexes and save them into files",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return crawl(cmd.Context())
+		},
+	}
+	buildCmd = &cobra.Command{
+		Use:   "build",
+		Short: "Build Java DB",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return build()
+		},
+	}
+)
+
+func init() {
 	userCacheDir, err := os.UserCacheDir()
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
 
-	crawl := flag.Bool("crawl", false, "crawl maven indexes and save them to files")
-	build := flag.Bool("build", false, "build db from stored maven index files")
-	cacheDir := flag.String("cache-dir", userCacheDir, "cache dir")
-	flag.Parse()
+	rootCmd.PersistentFlags().StringVar(&cacheDir, "cache-dir", userCacheDir, "config file (default is $HOME/.cobra.yaml)")
+	rootCmd.PersistentFlags().IntVar(&limit, "limit", 1000, "max parallelism")
 
-	if !*crawl && !*build {
-		return xerrors.Errorf("command not selected")
+	rootCmd.AddCommand(crawlCmd)
+	rootCmd.AddCommand(buildCmd)
+}
+
+func crawl(ctx context.Context) error {
+	indexesDir := filepath.Join(cacheDir, "trivy-java-db", "indexes")
+	log.Printf("crawl maven repository and save indexes in %s", indexesDir)
+	c := crawler.NewCrawler(crawler.Option{
+		Limit: int64(limit),
+		Dir:   indexesDir,
+	})
+	if err := c.Crawl(ctx); err != nil {
+		return xerrors.Errorf("crawl error: %w", err)
 	}
+	return nil
+}
 
-	dbDir := filepath.Join(*cacheDir, "trivy-java-db")
-
-	if *crawl {
-		indexesDir := filepath.Join(dbDir, "indexes")
-		log.Printf("crawl maven repository and save indexes in %s", indexesDir)
-		c := crawler.NewCrawler(crawler.Option{
-			Limit: 1000,
-			Dir:   indexesDir,
-		})
-		ctx := context.Background()
-		if err = c.Crawl(ctx); err != nil {
-			return xerrors.Errorf("crawl error: %w", err)
-		}
+func build() error {
+	dbDir := filepath.Join(cacheDir, "trivy-java-db")
+	log.Printf("Database directory: %s", dbDir)
+	dbc, err := db.New(dbDir)
+	if err != nil {
+		return xerrors.Errorf("db create error: %w", err)
 	}
-
-	if *build {
-		log.Printf("The database directory: %s", dbDir)
-		dbc, err := db.New(dbDir)
-		if err != nil {
-			return xerrors.Errorf("db create error: %w", err)
-		}
-		if err = dbc.Init(); err != nil {
-			return xerrors.Errorf("db init error: %w", err)
-		}
-		meta := metadata.New(dbDir)
-		if err = dbc.BuildDB(meta); err != nil {
-			return xerrors.Errorf("insert indexes to db error: %w", err)
-		}
+	if err = dbc.Init(); err != nil {
+		return xerrors.Errorf("db init error: %w", err)
 	}
-
+	meta := metadata.New(dbDir)
+	if err = dbc.BuildDB(meta); err != nil {
+		return xerrors.Errorf("insert indexes to db error: %w", err)
+	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/PuerkitoBio/goquery v1.5.1
 	github.com/hashicorp/go-retryablehttp v0.7.2
+	github.com/spf13/cobra v1.6.1
 	github.com/stretchr/testify v1.8.1
 	golang.org/x/sync v0.0.0-20201020160332-67f06af15bc9
 	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
@@ -18,10 +19,12 @@ require (
 	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/kballard/go-shellquote v0.0.0-20180428030007-95032a82bc51 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20200410134404-eec4a21b6bb0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	golang.org/x/mod v0.3.0 // indirect
 	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
 	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
