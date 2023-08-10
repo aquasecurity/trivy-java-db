7cc48c1fe570fdb17ed45bc5e2a6155d11920f4f
namandf <naman@deepfactor.io>
2023-08-10 14:00:37 +0530

    Add a utils file in crawler package

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -5,7 +5,6 @@ import (
 	"encoding/hex"
 	"encoding/xml"
 	"fmt"
-	"hash/fnv"
 	"io"
 	"log"
 	"net/http"
@@ -506,29 +505,4 @@ func (c *Crawler) prepareClassifierData() cmap.ConcurrentMap[string, License] {
 	}
 
 	return filesLicenseMap
-}
-
-func getLicenseKey(l License) string {
-	if len(l.URL) > 0 && strings.HasPrefix(l.URL, "http") {
-		return hash(l.URL)
-	}
-	return hash(l.Name)
-}
-
-func getPomURL(baseURL, artifactID, version string) string {
-	pomFileName := fmt.Sprintf("/%s-%s.pom", artifactID, version)
-	return baseURL + version + pomFileName
-}
-
-func hash(s string) string {
-	h := fnv.New32a()
-	h.Write([]byte(s))
-	return fmt.Sprint(h.Sum32())
-}
-
-func min(a int, b int) int {
-	if a > b {
-		return b
-	}
-	return a
 }
