082bc9523965f087a4418a498096c0a9ff29068d
knqyf263 <knqyf263@gmail.com>
2025-05-08 10:59:06 +0400

    refactor(crawler): remove unused Metadata and Versioning types
    - Deleted the `Metadata` and `Versioning` structs from `types.go` as they are no longer utilized in the codebase.
    - This change helps to simplify the code and reduce clutter, improving maintainability.

--- pkg/crawler/types.go
+++ pkg/crawler/types.go
@@ -1,18 +1,5 @@
 package crawler
 
-type Metadata struct {
-	GroupID    string     `xml:"groupId"`
-	ArtifactID string     `xml:"artifactId"`
-	Versioning Versioning `xml:"versioning"`
-}
-
-type Versioning struct {
-	//Latest      string   `xml:"latest"`
-	//Release     string   `xml:"release"`
-	Versions    []string `xml:"versions>version"`
-	LastUpdated string   `xml:"lastUpdated"`
-}
-
 type GCSListResponse struct {
 	NextPageToken string   `json:"nextPageToken,omitempty"`
 	Items         []Item   `json:"items,omitempty"`
